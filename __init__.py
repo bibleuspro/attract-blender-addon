04f49441c6fe9347cfc9813a95931d7fe4e2f5a3
bibleuspro <mediapastor@yahoo.com>
2017-02-10 00:12:04 -0700

    Revert to use blender id as string

--- __init__.py
+++ __init__.py
@@ -116,6 +116,7 @@
 
 
 
+
              blender_id_text = "This add-on requires Blender ID" 
 
 
@@ -123,41 +124,26 @@
 
 
                  "installed and activated") 
-
-
--        elif blender_id_profile.unique_id == 0: 
-
-
-+        elif blender_id_profile.unique_id == "": 
-
-
-             blender_id_icon = 'ERROR' 
+    elif blender_id_profile.unique_id == "": 
+      nder_id_icon = 'ERROR' 
 
 
              blender_id_text = "You are logged out." 
 
 
              blender_id_help = ("To login, " 
- 
-@@ -136,7 +136,7 @@ def draw(self, context):
-
-
-         # options for attract 
+                                
+                                # options for attract 
 
 
          sub = layout.column() 
 
 
          sub.enabled = (blender_id_profile != None 
-
-
--            and blender_id_profile.unique_id != 0) 
-
-
-+            and blender_id_profile.unique_id != "") 
-
-
-         sub.prop(self, "attract_server") 
+                        
+                        and blender_id_profile.unique_id != "") 
+                                
+                                sub.prop(self, "attract_server") 
 
 
          sub.operator("attract.credentials_update") 
